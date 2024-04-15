08f4d2f2fbd51388c53a3c6a18a8d66c1d263146
gjbakker <g.j.bakker@st.hanze.nl>
2024-04-15 11:32:23 +0200

    made a few changes to the explaination

--- __init__.py
+++ __init__.py
@@ -1,4 +1,16 @@
-from .kerstman import kerstboom
+"""
+Dit classifier model is door Jort Gommers en Giel Bakker gevormd. In dit project zijn er twee .ipynb bestanden gevormd.
+In de comp.ipynb en machine_log_delete.ipynb zijn dezelfde stappen genomen in het vinden van het machine learning model met de
+hoogste accuracy. Het enige verschil is dat de vraagtekens bij comp.ipynb zijn vervangen door de gemiddelde waarde van deze feature,
+en dat de machine_log_delete.ipynb de instances verwijderd wanneer er een vraagteken is gevonden.
+Alhoewel er in de machine_log_delete.ipynb met de LinearDiscriminant analysis een hogere test accuracy is behaald, is deze niet werkend
+gekregen in de class. Dit komt doordat de test data voor de prognose10jaar altijd vraagetekens bevat. Hierdoor moet deze features worden
+verwijderd, alleen zorgt dit er ook voor dat de PCA niet goed verloopt. Zonder de PCA wordt er een lagere accuracy behaald, waardoor
+ervoor gekozen is om het model te vormen door bij de missende waarde het gemiddelde in te vullen.
+Er wordt een RandomForestClassifier model gebruikt in de predict class.
+"""
+
+from kerstman import kerstboom
 
 def model_factory():
     return kerstboom(true_or_false=True)
--- kerstman.py
+++ kerstman.py
@@ -10,25 +10,42 @@ import numpy as np
 
 class kerstboom:
     def __init__(self, true_or_false=False):
+        """
+        Deze functie dient als opvulling voor de class.
+        Deze heeft geen verdere doel voor het machine learning model.
+        :param true_of_false: Deze waarde kan True of False zijn en veranderd
+        hiermee de self naar True of False.
+        """
         self.true_or_false = true_or_false
-       
+
     def predict(self,  filename='competition_test.csv'):
-       
+        """
+        Deze functie ontvangt een test dataset voor de prognose10jaar
+        van een hartziekte, en doet met een eerder geselecteerd machine learning
+        model een voorspelling aan de hand van deze dataset.
+        Voor de voorspelling, wordt de data eerst omgezet naar een bruikbare
+        formaat voor het machine learning model. Ook wordt er een PCA uitgevoerd
+        op de gegeven test data.
+        :param filename: Dit is de path naar het bestand met erin de test data,
+        waarop een voorspelling moet worden gedaan.
+        :output prediction: Er wordt een lijst gevormd met erin de voorspellingen
+        van het machine learning model. 
+        """
         data = pd.read_csv(filename)
         data['prognose10jaar'] = data['prognose10jaar'].astype('category')
         # Preprocessing moet hierna komen
         columns_to_replace = ['hypertensie', 'hartinfarct', 'diabetes', 'nierziekte']
- 
+
         for column in columns_to_replace:
             data[column] = data[column].replace({'-': 0, '+': 1})
         data = data.drop('Individu-ID', axis=1)
-       
+
         data = data.drop('prognose10jaar', axis=1)
         data['geslacht'] = data['geslacht'].replace({'M': 1, 'V': 0})
- 
+
         # Maak een LabelEncoder object
         label_encoder = LabelEncoder()
-       
+
         # Pas labelencodering toe op 'opleidingsniveau'
         data['opleidingsniveau'] = label_encoder.fit_transform(data['opleidingsniveau'])
 
@@ -43,27 +60,27 @@ class kerstboom:
 
         # Vervang NaN-waarden door de gemiddelde waarden per kolom
         data.fillna(column_means, inplace=True)
- 
+
         scaled = StandardScaler().fit_transform(data.select_dtypes(include='number'))
-       
+
         # Fit PCA on the scaled data
         pca = PCA().fit(scaled)
-       
+
         # Transform the scaled data using PCA
         components = pca.transform(scaled)
-       
+
         # Iterate over the first 4 principal components
         for n in range(4):
             # Create attribute names for the principal components
             attribute = f"PC-{n+1}"
-           
+
             # Add new columns to the 'stroke' DataFrame with the principal component values
             data[attribute] = components[:, n]
- 
+
         # y = data.iloc[:, :-1]
         X = data.to_numpy()
         modelfile = pkg_resources.resource_filename(__name__, 'kerstboom.pkl')
-       
+
         with open(modelfile, 'rb') as filehandle:
             model = pickle.load(filehandle)
         yhat = model.predict(X)
